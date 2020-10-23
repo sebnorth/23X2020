b786fe494bf8605b5ee31f9fff8528d20270093c
sebnorth <sebnorth@gmail.com>
2020-10-23 15:59:12 +0200

    Dodaj pliki projektów.

---
+++ RokPrzestepny/Obliczenia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RokPrzestepny
+{
+    class Obliczenia
+    {
+        public static void CzyDanyRokJestPrzestepny(int rok_parametr) {
+
+            if ((rok_parametr % 4 == 0 && rok_parametr % 100 != 0) || rok_parametr % 400 == 0)
+                Console.WriteLine("Rok {0} jest przestępny", rok_parametr);
+            else
+                Console.WriteLine("Rok {0} nie jets przestępny", rok_parametr);
+        }
+    }
+}
---
+++ RokPrzestepny/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RokPrzestepny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Podaj rok: ");
+            //int.TryParse(Console.ReadLine(), out int rok);
+
+            Obliczenia.CzyDanyRokJestPrzestepny(2020);
+            Obliczenia.CzyDanyRokJestPrzestepny(2021);
+            Obliczenia.CzyDanyRokJestPrzestepny(2022);
+            Obliczenia.CzyDanyRokJestPrzestepny(2023);
+            Obliczenia.CzyDanyRokJestPrzestepny(2024);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
